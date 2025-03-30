473a5ccfa0b8ea0ec2e0051b377679972e7e2fdf
Gustavo Carrasco Villosalda <U24236873@utp.edu.pe>
2025-03-29 21:33:35 -0500

    Update Eje2_VolumendelCilindro.java

--- src/tareasemana1/Eje2_VolumendelCilindro.java
+++ src/tareasemana1/Eje2_VolumendelCilindro.java
@@ -11,14 +11,14 @@ import java.util.Scanner;
 public class Eje2_VolumendelCilindro {
   public static void main(String[] args) {
         double radio,altura,volumen;
-        Scanner scanner = new Scanner(System.in);
+        Scanner lector = new Scanner(System.in);
         
         // Solicitar radio 
         System.out.print("Ingrese el radio del cilindro: ");
-        radio = scanner.nextDouble();
+        radio = lector.nextDouble();
         // Solicitar la altura 
         System.out.print("Ingrese la altura del cilindro: ");
-        altura = scanner.nextDouble();
+        altura = lector.nextDouble();
         
         // Calcular el volumen
         volumen = Math.PI * Math.pow(radio, 2) * altura; 
